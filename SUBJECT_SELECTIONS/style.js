28cc5a33ecde252dfda55c14673be4188dbedd1c
Suyash Adhikari <suyashadhikari99@gmail.com>
2022-12-15 19:06:20 +0545

    Deletion
    Deleted all old files

--- SUBJECT_SELECTIONS/style.js
+++
@@ -1,5 +0,0 @@
-function dropdown() {
-    const buttonElement = document.getElementById("dropDownDisplay");
-    console.log("test");
-    document.getElementById("dropDownDisplay").classList.remove("hide");
-  }
